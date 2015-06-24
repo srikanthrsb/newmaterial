0000f67eb8b5ac600e9185bab71bed5a03993d4f
buchale.reddy <buchale.reddy@brillio.com>
2015-06-24 16:25:59 +0530

    Tabs are added

--- app/src/main/java/com/sptuts/newmaterial/MainActivity.java
+++ app/src/main/java/com/sptuts/newmaterial/MainActivity.java
@@ -1,17 +1,38 @@
 package com.sptuts.newmaterial;
 
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 //This is just a sample comment
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
+
+    FloatingActionButton fab;
+    TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setupToolbar();
+        setupTabLayout();
+        setupFAB();
+
+    }
+
+    private void setupTabLayout() {
+        tabLayout = (TabLayout)findViewById(R.id.tabLayout);
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+        tabLayout.addTab(tabLayout.newTab().setText("Data"));
+        tabLayout.addTab(tabLayout.newTab().setText("Gain"));
+        tabLayout.addTab(tabLayout.newTab().setText("Loss"));
     }
 
     @Override
@@ -19,6 +40,25 @@ public class MainActivity extends ActionBarActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+    private void setupFAB() {
+        fab = (FloatingActionButton)findViewById(R.id.fab);
+        fab.setOnClickListener(fabLstnr);
+    }
+
+    View.OnClickListener fabLstnr = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Snackbar.make(findViewById(R.id.rootLayout), "Snackbar called by Fab", Snackbar.LENGTH_LONG)
+                    .setAction("UNDO",this)
+                    .show();
+        }
+    };
+
+    private void setupToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 
     @Override
