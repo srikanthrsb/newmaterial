804774a179c409390b94ceac082bf87581d21a22
buchale.reddy <buchale.reddy@brillio.com>
2015-06-26 17:42:49 +0530

    EditText Started

--- app/src/main/java/com/sptuts/newmaterial/fragments/Secondfragment.java
+++ app/src/main/java/com/sptuts/newmaterial/fragments/Secondfragment.java
@@ -3,17 +3,29 @@ package com.sptuts.newmaterial.fragments;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.support.design.widget.TextInputLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.sptuts.newmaterial.R;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class Secondfragment extends android.support.v4.app.Fragment {
 
+
+    @InjectView(R.id.tilLName) TextInputLayout tilName;
+    @InjectView(R.id.tilAge) TextInputLayout tilAge;
+    @InjectView(R.id.tilPhone) TextInputLayout tilPhone;
+    @InjectView(R.id.edAge) EditText edAge;
+    @InjectView(R.id.edPhone) EditText edPhone;
 
     public Secondfragment() {
         // Required empty public constructor
@@ -24,8 +36,27 @@ public class Secondfragment extends android.support.v4.app.Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second, container, false);
+        View view= inflater.inflate(R.layout.fragment_second, container, false);
+        ButterKnife.inject(this,view);
+        return view;
     }
 
 
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        tilName.setErrorEnabled(true);
+        tilName.setError("Min 2 char required");
+        edAge.setError("Required");
+        tilPhone.setErrorEnabled(true);
+        tilPhone.setError("Enter a phone number");
+        edPhone.setError("Required");
+
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        ButterKnife.reset(this);
+    }
 }
