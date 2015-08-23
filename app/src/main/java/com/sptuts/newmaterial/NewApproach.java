c15beb857ccc0543da40b63a1e8e5d43975d180a
Buchale <sbsdevs@gmail.com>
2015-08-24 02:20:53 +0530

    Contact Layout Started

---
+++ app/src/main/java/com/sptuts/newmaterial/NewApproach.java
@@ -0,0 +1,77 @@
+package com.sptuts.newmaterial;
+
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import com.sptuts.newmaterial.adapters.PagerAdapter;
+
+public class NewApproach extends AppCompatActivity {
+
+    TabLayout tabLayout;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_approach);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //http://www.jayrambhia.com/blog/contacts-demo/
+        setSupportActionBar(toolbar);
+        setupTabLayout();
+    }
+
+    /*@Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_new_approach, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+*/
+
+    private void setupTabLayout() {
+        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.ic_data));
+        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.ic_up));
+        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.ic_down));
+
+
+        final ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        final PagerAdapter adapter = new PagerAdapter(getSupportFragmentManager(), tabLayout.getTabCount());
+        viewPager.setAdapter(adapter);
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition(), true);
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+    }
+
+}
--- app/src/main/java/com/sptuts/newmaterial/adapters/PagerAdapter.java
+++ app/src/main/java/com/sptuts/newmaterial/adapters/PagerAdapter.java
@@ -5,8 +5,6 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 
 import com.sptuts.newmaterial.fragments.Thirdfragment;
-import com.sptuts.newmaterial.fragments.TweetFragment;
-import com.sptuts.newmaterial.fragments.TweetList;
 
 /**
  * Created by SHRI on 6/24/2015.
@@ -26,10 +24,10 @@ public class PagerAdapter extends FragmentPagerAdapter {
         switch (position) {
             case 0:
                 //Firstfragment firstFrag = new Firstfragment();
-                return new TweetFragment();
+                return new Thirdfragment(); //TweetFragment();
             case 1:
                 //Secondfragment secondFrag = new Secondfragment();
-                return new TweetList();
+                return new Thirdfragment(); //TweetList();
             case 2:
                 return new Thirdfragment();
             default:
